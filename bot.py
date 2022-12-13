fee6634dbe1ab48d62b0d49bec3fec8f6a706e53
eshulman2 <eshulman@redhat.com>
2022-12-13 10:18:31 +0200

    move to slack bolt

--- bot.py
+++ bot.py
@@ -1,77 +1,61 @@
-
-import slack
-from flask import Flask
-from slackeventsapi import SlackEventAdapter
 import os
-import logging
 from tinydb import TinyDB, Query
-import re
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
 
-SLACK_TOKEN = os.environ['SLACK_TOKEN']
-SIGNING_SECRET = os.environ['SIGNING_SECRET']
- 
-app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(SIGNING_SECRET, '/slack/events', app)
- 
-client = slack.WebClient(token=SLACK_TOKEN)
- 
-# initialize logger
-FORMAT = '%(asctime)s %(message)s'
-logging.basicConfig(format=FORMAT, filename='bot.log', filemode='a',)
-logger = logging.getLogger('slack_loggger')
+class db():
+    def __init__(self, db_file='./db.json'):
+        self.db = TinyDB(db_file)
+        self.set_up_table = self.db.table('set_up')
+        self.query = Query()
+    
+    def get_all_servers(self):
+        return '\n'.join([str(server) for server in self.set_up_table.all()])
 
-# Initialize DB and tables
-db = TinyDB('./db.json')
-users_table = db.table('users')
-set_up_table = db.table('set_up')
-# Create quary object for DB
-quary = Query()
+    def update_server_user(self, set_up, user_name):
+        self.set_up_table.upsert(
+                {'setup': set_up, 'user': user_name, 'in_use': True},
+                self.quary.setup == set_up)
 
-@ slack_event_adapter.on('message')
-def message(payload):
-    print(payload)
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
- 
-    if re.compile(r'bootstrap db.*').match(text):
-        try:
-            set_up_table.insert_multiple(text.replace('bootstrap db ', ''))
-        except Exception as err:
-            logger.warning(f'error with boostrap database: {err}')
-            client.chat_postMessage(channel=channel_id,
-                                    text=f"something went wrong {err}")
-    elif re.compile(r'using.*').match(text):
-        try:
-            setup = text.replace('using ', '')
-            set_up_table.upsert(
-                {'setup': setup, 'user': user_id}, quary.setup == setup)
-        except Exception as err:
-            logger.warning(f'error with updating used server: {err}')
-            client.chat_postMessage(channel=channel_id,
-                                    text=f"something went wrong {err}")
-    elif re.compile(r'free setup.*').match(text):
-        try:
-            setup = text.replace('free setup ', '')
-            set_up_table.upsert(
-                {'setup': setup, 'user': ''}, quary.setup == setup)
-        except Exception as err:
-            logger.warning(f'error with updating used server: {err}')
-            client.chat_postMessage(channel=channel_id,
-                                    text=f"something went wrong {err}")
-    elif re.compile(r'show all.*').match(text):
-        try:
-            for server in set_up_table.all():
-                client.chat_postMessage(channel=channel_id,
-                                        text=str(server))
-        except Exception as err:
-            logger.warning(f'error with updating used server: {err}')
-            client.chat_postMessage(channel=channel_id,
-                                    text=f"something went wrong {err}")
-    else:
-        client.chat_postMessage(channel=channel_id,
-                                    text=f"help!")
+    def free_up_server(self, set_up):
+        self.set_up_table.upsert({'setup': set_up, 'user': None,
+                'in_use': False},
+                self.quary.setup == set_up)
+
+    def list_free_servers(self):
+        '\n'.join([str(server) for server in 
+                    self.set_up_table.search(self.quary.in_use == 'False')])
+
+
+DB = db()
+
+# Install the Slack app and get xoxb- token in advance
+app = App(token=os.environ.get('BOT_TOKEN'))
+
+# @app.event("app_mention")
+# def event_test(say, body):
+#     print(body['elements']['text'])
+#     # say("Hi there!")
+
+@app.command("/show-all-setups")
+def hello_command(ack, command, respond):
+    ack()
+    respond(DB.get_all_servers())
+
+@app.command("/use-server")
+def hello_command(ack, command, respond):
+    DB.update_server_user(command["text"], command["user"])
+    ack("ack")
+    
+@app.command("/free-server")
+def hello_command(ack, command, respond):
+    DB.update_server_user(command["text"])
+    ack("ack")
+
+@app.command("/list-free")
+def hello_command(ack, command, respond):
+    ack(DB.list_free_servers())
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    # 'xapp- (zap) App Token'
+    SocketModeHandler(app, os.environ.get('APP_TOKEN')).start()
